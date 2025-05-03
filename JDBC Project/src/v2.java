37790d6d828c55cfdac317c0b111cd0383532dca
Tanisha Gupta <s24cseu0578@bennett.edu.in>
2025-05-03 17:58:56 +0530

    Update v2.java

--- JDBC Project/src/v2.java
+++ JDBC Project/src/v2.java
@@ -26,7 +26,7 @@ public class v2 {
             String url = "jdbc:mysql://localhost:3306/votingsystem";
 
             String user = "root";
-            String password = "your password here";
+            String password = "your_password_here";
             connection = DriverManager.getConnection(url, user, password);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Database connection error: " + e.getMessage());
